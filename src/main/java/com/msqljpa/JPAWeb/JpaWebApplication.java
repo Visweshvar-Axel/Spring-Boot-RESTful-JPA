b9948d476d6d8d5a5cfceabe5ae409db48d6f330
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-23 08:28:48 +0530

    initial commit

---
+++ src/main/java/com/msqljpa/JPAWeb/JpaWebApplication.java
@@ -0,0 +1,13 @@
+package com.msqljpa.JPAWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msqljpa/JPAWeb/JpaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msqljpa.JPAWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
