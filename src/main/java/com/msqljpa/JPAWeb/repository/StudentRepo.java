10803eb863bca04a548687841638cef9ec6892b6
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-23 09:58:03 +0530

    controller

--- src/main/java/com/msqljpa/JPAWeb/repository/StudentRepo.java
+++ src/main/java/com/msqljpa/JPAWeb/repository/StudentRepo.java
@@ -1,7 +1,13 @@
 package com.msqljpa.JPAWeb.repository;
 
 import com.msqljpa.JPAWeb.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepo extends JpaRepository<Student,Long> {
+//public class StudentRepo {
+//    @Autowired
+//    EntityManager em;
+
 }
